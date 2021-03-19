db0b6c1f7688c16a23d5655a43421ff0fb2201d1
Timothy7310 <penlist127@gmail.com>
2021-03-19 12:08:29 +0300

    create: added responsive for header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 $(function(){
 
-
+  $('#burger').click(function (event) {
+        $('.header__burger, .header__burger-span, .header__nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    
 const swiperMain = new Swiper('.main-slider', {
   loop: true,
 
